249fe89770942becc26924ef92a0a47efd1cc957
ark10806 <ark10806@gmail.com>
2021-05-08 20:16:19 +0900

    one hot

--- one_hot_enc.py
+++ one_hot_enc.py
@@ -1,69 +1,53 @@
-# from tensorflow import keras
-# from tensorflow.keras import Sequential
-# from tensorflow.keras.layers import Dense, Activation
-# from keras import optimizers
-# from keras import layers, models
-# import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from keras import optimizers
+from keras import layers, models
+import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# pd.options.display.max_rows=15
+pd.options.display.max_rows=15
 
 white_wine = pd.read_csv('./wine_data/winequality-white.csv')
 red_wine = pd.read_csv('./wine_data/winequality-red.csv')
 
-# def plot_loss(history):
-#     plt.plot(history.history['loss'])
-#     plt.plot(history.history['val_loss'])
-#     plt.title('Model Loss')
-#     plt.ylabel('Loss')
-#     plt.xlabel('Epoch')
-#     plt.legend(['Train','Test'], loc=0)
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train','Test'], loc=0)
 
-# def plot_acc(history):
-#     plt.plot(history.history['accuracy'])
-#     plt.plot(history.history['val_accuracy'])
-#     plt.title('Model acc')
-#     plt.ylabel('Accuracy')
-#     plt.xlabel('Epoch')
-#     plt.legend(['Train','Test'], loc=0)
+def plot_acc(history):
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model acc')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train','Test'], loc=0)
 
-# def generate_data(df: pd.DataFrame, t_r: float):
-    # total_data = df.to_numpy()
-    # normalize(total_data)
+def generate_data(df: pd.DataFrame, t_r: float):
+    total_data = df.to_numpy()
+    normalize(total_data)
 
-    # np.random.shuffle(total_data)
-    # n_train_set = int(np.shape(total_data)[0] * t_r)
+    np.random.shuffle(total_data)
+    n_train_set = int(np.shape(total_data)[0] * t_r)
 
-    # x_train = total_data[:n_train_set, :-1]
-    # y_train = total_data[:n_train_set, -1:]
+    x_train = total_data[:n_train_set, :-1]
+    y_train = total_data[:n_train_set, -1:]
 
-    # x_test = total_data[n_train_set:, :-1]
-    # y_test = total_data[n_train_set:, -1:]
+    x_test = total_data[n_train_set:, :-1]
+    y_test = total_data[n_train_set:, -1:]
 
-    # return x_train, y_train, x_test, y_test
+    return x_train, y_train, x_test, y_test
 
 
 def normalize(total_data: np.array) -> None:
     for i in range(np.shape(total_data)[1]-1):
         col_zero_base = total_data[:,i] - total_data[:,i].min() + 1e-4
         total_data[:,i] = ( col_zero_base ) / ( col_zero_base.max() )
-
-one_hot_codes = np.array()
-def make_label():
-    tmp = np.zeros(shape=(10,))
-    for i in range(10):
-        tmp[i] = 1
-        one_hot_codes = np.append(one_hot_codes, tmp)
-        tmp[i] = 0
-
-make_label()
-print(one_hot_codes)
-exit(0)
-
-def one_hot_enc(y_label: np.array) -> np.array:
-    pass
-
 
 x_train, y_train, x_test, y_test = generate_data(white_wine, 0.7)
 
